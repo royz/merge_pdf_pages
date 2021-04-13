66b3f5c97373fab5c7b1377abb20631168d550f2
Rajorshi Roy <rajorshi.me@gmail.com>
2021-04-14 01:16:31 +0530

    fix merging for pdf where number of labels are between 1 and 6

--- merge_pages.py
+++ merge_pages.py
@@ -1,10 +1,8 @@
 import os
 import sys
 import glob
-from pprint import pprint
-
-import PyPDF2
 import shutil
+import PyPDF2
 
 
 def merge(pdf_source=None, pdf_dest=None, page_limit=None):
@@ -24,8 +22,6 @@ def merge(pdf_source=None, pdf_dest=None, page_limit=None):
         dir_name, file_name = os.path.split(pdf_source)
         pdf_dest = os.path.join(dir_name, 'merged_labels', file_name)
         os.makedirs(os.path.join(dir_name, 'merged_labels'), exist_ok=True)
-        print(pdf_dest)
-        return
 
     if not page_limit:
         page_limit = sys.maxsize
@@ -36,6 +32,10 @@ def merge(pdf_source=None, pdf_dest=None, page_limit=None):
     # add all labels. if the specified limit is more than available
     # labels then just add all the labels
     NUM_OF_PAGES = min(reader.getNumPages(), page_limit)
+
+    # divide the labels into segments of 6 labels per page
+    segmented_pages = [list(range(NUM_OF_PAGES))[offset:offset + 6] for offset in range(0, NUM_OF_PAGES, 6)]
+    print('page segments:', segmented_pages)
 
     # if the pdf has only one label then just copy the same pdf as output
     if NUM_OF_PAGES == 1:
@@ -52,25 +52,29 @@ def merge(pdf_source=None, pdf_dest=None, page_limit=None):
     # create the pdf writer object
     pdf_writer = PyPDF2.PdfFileWriter()
 
-    # for every 6 labels, create a new page
-    for _ in range(0, NUM_OF_PAGES, 6):
+    for dest_page in segmented_pages:
         new_pdf_page = PyPDF2.pdf.PageObject.createBlankPage(None, new_page_width, new_page_height)
-
         translate_y = None
-        for i in range(6):
+
+        for idx, source_page_num in enumerate(dest_page):
             # update the translate y property for each 2 labels
-            if i % 2 == 0:
-                translate_y = data_height * i / 2
-            translate_x = ((i + 1) % 2) * data_width
+            if source_page_num % 2 == 0:
+                translate_y = data_height * (3 - idx // 2 - 1)
+            translate_x = (source_page_num % 2) * data_width
 
             # add the current label to the pdf page
-            new_pdf_page.mergeScaledTranslatedPage(
-                reader.getPage(NUM_OF_PAGES - i - 1),
-                scale=1,
-                tx=translate_x,
-                ty=translate_y
-            )
-        # add this page to writer object
+            try:
+                new_pdf_page.mergeScaledTranslatedPage(
+                    reader.getPage(source_page_num),
+                    scale=1,
+                    tx=translate_x,
+                    ty=translate_y
+                )
+            except IndexError:
+                print('error')
+                continue
+
+            # add this page to writer object
         pdf_writer.addPage(new_pdf_page)
 
     # save the merged pdf in source dir
@@ -79,6 +83,10 @@ def merge(pdf_source=None, pdf_dest=None, page_limit=None):
 
 
 if __name__ == '__main__':
-    files = glob.glob('./arco_labels/*')
-    for file in files:
-        merge(file)
+    # files = glob.glob('./arco_labels/*')
+    # for file in files:
+    #     merge(file)
+
+    # merge('arco_labels/2021032609111501375592.pdf')
+    # merge('labels_example.pdf')
+    merge('labels_12_pages.pdf')
